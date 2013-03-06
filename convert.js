d953a5485924af49dde6005c72a3abc88880e0fd
jabsters <jaby99@gmail.com>
2013-03-06 14:33:40 -0700

    Improved convert.js
    Looking for RD_PARM1=.

--- convert.js
+++ convert.js
@@ -1,8 +1,17 @@
 $j = jQuery.noConflict();
 
-var url;
+var url,positionRegular,positionLS;
 $j('.autolinker_link').each(function(index){
-    url = decodeURIComponent($j(this).attr('href'));
-    $j(this).attr('href',url.replace('&amp;','&').slice(url.indexOf('url=')+4));
-    $j(this).removeClass('autolinker_link');
+    url = decodeURIComponent(decodeURIComponent($j(this).attr('href')).replace('&amp;','&'));
+
+    positionLS = url.indexOf('RD_PARM1=');
+    positionRegular = url.indexOf('url=');
+
+    if (positionLS !== -1) {
+        $j(this).attr('href',url.substring(positionLS+9));
+        $j(this).removeClass('autolinker_link');
+    } else if (positionRegular !== -1) {
+        $j(this).attr('href',url.substring(positionRegular+4));
+        $j(this).removeClass('autolinker_link');
+    }
 });
